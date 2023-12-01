04ba019f9b1b748fbca8ba2e7c72a6e3a570cd21
LAPTOP-GU8HKD49\ASUS <Phuongnguyen27102003@gmail.com>
2023-12-01 20:00:35 +0700

    First commit

---
+++ TicketLand_project/Controllers/DangkiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicketLand_project.Controllers
+{
+    public class DangkiController : Controller
+    {
+        // GET: Dangki
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TicketLand_project/Controllers/DangnhapController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicketLand_project.Controllers
+{
+    public class DangnhapController : Controller
+    {
+        // GET: Dangnhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
